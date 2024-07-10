2e818377180cbc9dadfdb5d48603280f1ba6a00f
Paweł Pietras <pawel.pietras144@gmail.com>
2024-07-10 14:53:33 +0200

    env and package config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      STRIPE_KEY: process.env.STRIPE_KEY,
+      STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    },
+  },
+});
